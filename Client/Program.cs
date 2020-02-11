1fd3bc34bba15b2c1f33d2b3a7676b6797c0e3cc
Dainius Daukševičius <dainius.dauksevicius@gmail.com>
2020-02-11 22:19:05 +0200

    update to blazor v3.2

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,21 +1,59 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using MoneyManager.Client;
-using System;
+using MoneyManager.Client.Services;
+using MoneyManager.Client.State;
+using MoneyManager.Client.State.Reducers;
 using System.Threading.Tasks;
 
 namespace MoneyManager
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var webHost = CreateHostBuilder(args).Build();
-            webHost.Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.RootComponents.Add<App>("app");
+            ConfigureServices(builder.Services);
+
+            
+            var host = builder.Build();
+            //host.Configuration.
+            
+            await host.RunAsync();
         }
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+
+            var initialState = new AppState();
+            var store = new Store<AppState>(RootReducer.Reducer, initialState);
+            services.AddSingleton(store);
+
+            services.AddScoped<Bootstrapper>();
+
+            services.AddScoped<ModalService>();
+            services.AddScoped<MessageService>();
+
+
+            services.AddAuthorizationCore();
+            services.AddScoped<JwtAuthStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(provider =>
+                provider.GetRequiredService<JwtAuthStateProvider>()
+            );
+            services.AddScoped<HttpClient>();
+            services.AddScoped<AuthService>();
+            services.AddScoped<UserService>();
+            services.AddScoped<LocalStorage>();
+            services.AddScoped<SessionStorage>();
+
+            services.AddScoped<AccountService>();
+            services.AddScoped<CurrencyService>();
+            services.AddScoped<CategoryService>();
+            services.AddScoped<TransactionService>();
+        }
     }
 }
