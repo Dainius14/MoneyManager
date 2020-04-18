24f052a3681a182c9d7f7c432cb546abb0599990
Dainius Daukševičius <dainius.dauksevicius@gmail.com>
2020-04-18 10:56:59 +0300

    fix eslinter

--- ClientVue/.eslintrc.js
+++ ClientVue/.eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/typescript'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    parser: '@typescript-eslint/parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'curly': ['error', 'all'],
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single', { avoidEscape: true }],
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/typescript'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'curly': ['warn', 'all'],
+        'semi': ['warn', 'always'],
+        'quotes': ['warn', 'single', { avoidEscape: true }],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    }
 }
