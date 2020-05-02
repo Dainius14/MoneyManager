6c1d59672bbb8d3ff5dc247b643c8aa0e53dd67f
Dainius Daukševičius <dainius.dauksevicius@gmail.com>
2020-05-02 19:43:43 +0300

    limit used build memory for rpi builds

--- ClientVue/vue.config.js
+++ ClientVue/vue.config.js
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const os = require('os');
+
 module.exports = {
     'transpileDependencies': [
         'vuetify'
     ],
     configureWebpack: {
         devtool: 'source-map'
-    }
+    },
+    chainWebpack: config => {
+        config
+            .plugin('fork-ts-checker')
+            .tap(args => {
+                const totalmem = Math.floor(os.totalmem()/1024/1024); //get OS mem size
+                args[0].memoryLimit = totalmem > 1024 ? 2048 : 512;
+                return args;
+            });
+    },
 };
